313b6a29373641fd9c101defd097293ed95a5058
jimmyhernandez1 <70756425+jimmyhernandez1@users.noreply.github.com>
2020-11-20 10:23:39 -0500

    Update consolidation_sem.m

--- consolidation_sem.m
+++ consolidation_sem.m
@@ -1,4 +1,4 @@
-function consolidation_semantique = consolidation_sem(condition)
+function consolidation_semantique = consolidation_sem(phase)
 %Cette fonction est conçue pour mesurer la consolidation sémantique après
 %une nuit de sommeil. 
 %La première condition (1)  est la phase de mémorisation. Elle consiste à présenter
@@ -14,7 +14,6 @@ function consolidation_semantique = consolidation_sem(condition)
 
 %Nettoyer le workspace et l'écran des précédentes opérations
 clc;
-%clear all;
 close all;
 
 %Amorce le générateur de nombres aléatoires afin que tous les nombres générés soient vraiment "aléatoires"
@@ -23,26 +22,61 @@ rng('shuffle');
 screens=Screen('Screens');
 screenNumber=max(screens);
 
+%Initialiser la fonction de structure pour éventuelement entrer les
+%informations du participant et ses TR
+info_par = struct;
+
+%Demander numéro de participant et âge dans la Command Window
+num_par = input('Numéro de participant: ');
+info_par.NumParticipant = num_par;
+age_par = input('Âge: ');
+info_par.AgeParticipant = age_par;
+
+%Initialiser des variables pour enregistrer par la suite si le participant
+%a complété les tâches 1 et/ou 2
+task_1 = 0;
+task_2 = 0;
+
+%Matrices contenant les noms des catégories sémantiques qui ont été
+%présentées (m_cat_fam) et les catégories sémantiques qui n'ont pas été
+%présentées (m_cat_unfam)
+m_cat_fam = ["Bureau" "Animal" "Sport" "Plante" "Breuvage" "Fruit" "Insecte" "Légume" "Meuble"];
+m_cat_unfam = ["Instrument de musique" "Véhicule" "Poisson" "Art" "Arme" "Outil" "Bâtiment" "Vêtement"];
+
+%Noms des fichiers d'images contenus dans leur catégorie sémantique
+%respective
+%m_cat_bureau = ["brocheuse.jpg";"stylo.jpg"];
+%m_cat_animal = ["Girafe.jpg";"lion.jpeg"];
+%m_cat_sport = ["tennis.jpeg";"vélo.jpeg"];
+%m_cat_plante = ["succulente.jpeg";"arbre.jpg"];
+%m_cat_breuvage = ["mojito.jpeg";"café.jpeg"];
+%m_cat_fruit = ["bleuet.jpeg";"kiwi.png"];
+%m_cat_insecte = ["abeille.jpeg";"fourmi.jpeg"];
+%m_cat_legume = ["carotte.jpeg";"brocoli.jpg"];
+%m_cat_meuble = ["canapé.jpg";"table.jpg"];
+
+%Matrice résultant de la concaténation de toutes les matrices de catégories
+%sémantique 
+%m_cat_files = horzcat(m_cat_bureau,m_cat_animal,m_cat_sport,m_cat_plante,m_cat_breuvage,m_cat_fruit,m_cat_insecte,m_cat_legume,m_cat_meuble)
+
 %Si la condition est '1', voici ce que la fonction exécutera:
 % -> Présenter une paire non-mot + image, une à la fois à l’écran
 % -> Après 5 secondes, la paire non-mot + image change pour la prochaine
-if condition == 1
+if phase == 1
     
     %Ouvre une boîte de dialogue qui demande les informations du participant
     %dans l'optique de stocker ses réponses avec ses informations à des fins d'analyse
-    prompt = ({'Code de participant:','Âge:'});
-    dlgtitle = 'Information du participant';
-    dims = [1 40; 1 40]
-    info_participant = inputdlg(prompt,dlgtitle,dims)
+    task_1 = 1;
+    
     
     Screen('Preference','SkipSyncTests',1);
     
-    veteur_non_mots = ["tâfethor" "nognurel" "sarybêt" "lenêcheg" "gecolêt" "gugareg" "labèlel" "ramomet" "gasafod" "yarevat" "lêmobur" "ceduphèc" "cosonem" "botamom" "fanisîr" "quytamaf" "cynofép" "poçamec" "kasifoc" "cémacèm"];
-    image_mat = ["bleuet.jpeg","brocheuse.jpg","brocoli.jpg","café.jpeg","canapé.jpg","carotte.jpeg","ciseaux.jpg","fourmi.jpeg","Girafe.jpg","kiwi.png","lion.jpeg","mojito.jpeg","paresseux.jpeg","pizza.jpg","stylo.jpg","succulente.jpeg","tennis.jpeg","vélo.jpeg"];
+    veteur_non_mots = ["tâfethor" "sarybêt" "lenêcheg" "gecolêt" "gugareg" "labèlel" "ramomet" "gasafod" "yarevat" "lêmobur" "ceduphèc" "cosonem" "botamom" "fanisîr" "cynofép" "poçamec" "kasifoc" "cémacèm"];
+    image_mat = ["abeille.jpeg","bleuet.jpeg","brocheuse.jpg","brocoli.jpg","café.jpeg","canapé.jpg","carotte.jpeg","fourmi.jpeg","Girafe.jpg","kiwi.png","lion.jpeg","mojito.jpeg","stylo.jpg","succulente.jpeg","tennis.jpeg","vélo.jpeg","arbre.jpg",'table.jpg'];
     
     white = WhiteIndex(screenNumber);
     
-    [window,windowRect] = PsychImaging('OpenWindow',screenNumber,[0 0 0]);
+    [window,windowRect] = Screen('OpenWindow',screenNumber,[0 0 0]);
     
     Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
     
@@ -55,12 +89,11 @@ if condition == 1
     [xCenter, yCenter] = RectCenter(windowRect);
     
     for compteur = 1:length(image_mat)
-        %Image et mot qu'on présente dans ce qui serait normalement une
-        %boucle qui parcours la matrice d'image et la matrice de mots
+        %Image et mot qu'on présente
         image_presented = imread(image_mat(compteur));
         word_presented = veteur_non_mots(compteur);
         
-        %Redimensionner l'image à un ratio à un ratio qui correspond au 1/3
+        %Redimensionner l'image à un ratio qui correspond au 1/3
         %de la largeur et de la hauteur de l'écran pour chacune des images.
         %Ainsi, toutes les images présentées ont la même taille par rapport
         %à l'écran.
@@ -84,14 +117,33 @@ if condition == 1
         Screen('Flip',window);
         
         %Attendre 5 secondes avant de passer à l'image suivante
-        WaitSecs(2);
+        WaitSecs(1);
         compteur+1;
     end
     %It is very important to always end a script with this command if drawing is involved to send control of the screen back to MATLAB.
         Screen('CloseAll');
-elseif condition == 2
+        
+elseif phase == 2
+    
+    
     
 else
     disp("La condition entrée n'est pas valide. Veuillez entrer 1 ou 2, dépendamment de la condition");
     
 end
+
+%Les deux boucles suivantes vont changer les valeurs des variables de
+%complétion de tâche afin d'enregistrer si les tâches ont été complétées
+if task_1 == 1
+    info_par.Task1Completed = 'Completed';
+elseif task_1 == 0
+    info_par.Task1Completed = 'Not Completed';
+end
+
+if task_2 == 1
+    info_par.Task2Completed = 'Completed';
+elseif task_2 == 0
+    info_par.Task2Completed = 'Not Completed';
+end
+
+info_par
